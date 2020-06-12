2d5aa6c93b2ac1305b6ebbc799025bc8f51a48ad
JannisKohle <Jannis.Kohle@icloud.com>
2020-06-12 17:41:41 +0200

    created example get method

--- api/index.js
+++ api/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT;
+app.use(express.json());
+// const port = process.env.PORT;
 
-app.get("/", (req, res) => {
-    // do something
+app.get("/:mes", (req, res) => {
+    res.send(req.params.mes)
 });
 
-app.listen(port);
+app.listen(3000);
