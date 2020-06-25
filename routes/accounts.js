0167d3541067ccb4d2670c6b99b8ec5f3b1eac86
JannisKohle <Jannis.Kohle@icloud.com>
2020-06-25 12:17:09 +0200

    added / endpoints in routes/accounts.js

--- routes/accounts.js
+++ routes/accounts.js
@@ -3,6 +3,19 @@ const router = express.Router();
 
 
 
+router.get("/", (req, res) => {
+    res.send({"message": "very good response"});
+});
+router.post("/", (req, res) => {
+    res.send({"message": "very good response"});
+});
+router.patch("/", (req, res) => {
+    res.send({"message": "very good response"});
+});
+router.delete("/", (req, res) => {
+    res.send({"message": "very good response"});
+});
+
 router.get("/:accountId", (req, res) => {
     res.send({"message": "very good response"});
 });
