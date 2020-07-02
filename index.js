196286c8457001bf589abd3eeadf6b8798f6718a
JannisKohle <Jannis.Kohle@icloud.com>
2020-07-02 18:46:42 +0200

    started some DB work

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const express = require("express");
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const app = express();
+
+dotenv.config();
+
+mongoose.connect(process.env.DB_CONNECT, {useNewUrlParser: true}, { useUnifiedTopology: true }, () => {
+  console.log("connected to database");
+});
 
 const coursesRoutes = require("./routes/courses");
 const accountsRoutes = require("./routes/accounts");
